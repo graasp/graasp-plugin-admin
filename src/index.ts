84834fddc1a71fa78014df6b9371a2a3fb836185
pyphilia <pyphilia@gmail.com>
2022-08-12 09:33:14 +0200

    refactor: export plugins

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { default } from './service-api';
 
 export { AdminTaskManager } from './task-manager';
-
 export { AdminService } from './db-service';
+
+export { default as itemPlugin } from './services/items/service-api';
+export { default as memberPlugin } from './services/members/service-api';
